c16972d46941663e873e9788925785740261b7a2
Matt Hall <mhallrp@gmail.com>
2026-09-17 18:35:02 +0100

    State that this is a modified xwax, as GPLv3 requires
    The repo went public today and carried no sign it was a fork: upstream's
    README, upstream's copyright, and five files we wrote with no licence
    header at all (keylock.c/h, riaa.c/h, interpolate.h).
    
    GPLv3 section 5(a) requires a modified work to carry prominent notices
    saying it was modified and when. This adds them:
    
      - README leads with a fork notice, credits Mark Hills and xwax.org,
        disclaims affiliation, and lists what actually differs.
      - The five files we authored get the standard xwax GPLv3 header under
        Matt Hall's copyright, matching upstream's wording exactly.
    
    This matters beyond tidiness: the SD card image ships a modified GPLv3
    binary, so recipients are entitled to the corresponding source, and
    waxcode.co claims the engine is open source. Both now hold.

--- interpolate.h
+++ interpolate.h
@@ -1,4 +1,23 @@
 /*
+ * Copyright (C) 2026 Matt Hall <info@waxcode.co>
+ *
+ * This file is part of "xwax".
+ *
+ * "xwax" is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, version 3 as
+ * published by the Free Software Foundation.
+ *
+ * "xwax" is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+/*
  * Shared 4-point cubic interpolation.
  *
  * Was static in player.c. Moved here when keylock.c arrived so the varispeed resampler and the
--- riaa.c
+++ riaa.c
@@ -1,4 +1,23 @@
 /*
+ * Copyright (C) 2026 Matt Hall <info@waxcode.co>
+ *
+ * This file is part of "xwax".
+ *
+ * "xwax" is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License, version 3 as
+ * published by the Free Software Foundation.
+ *
+ * "xwax" is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+/*
  * Inverse RIAA equalisation and attenuation for the playback output.
  *
  * The playback curve a phono stage applies is defined by three time constants - 3180us, 318us and
